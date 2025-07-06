d8890a002b165a0f6835765f7b245b4ca2ae05a0
Cheezer1656 <118921295+Cheezer1656@users.noreply.github.com>
2025-07-06 13:30:29 -0700

    Create packet codec

--- protocol/src/lib.rs
+++ protocol/src/lib.rs
@@ -1,9 +1,56 @@
+use aes_gcm::{
+    AeadCore, Aes256Gcm, KeyInit,
+    aead::{Aead, OsRng},
+};
 use serde::{Deserialize, Serialize};
 
 pub mod packets;
 
 pub trait Packet: Serialize + for<'de> Deserialize<'de> + Send + Sync {
     const ID: u8;
+}
+
+#[derive(Default)]
+pub struct PacketCodec {
+    cipher: Option<Aes256Gcm>,
+}
+
+impl PacketCodec {
+    pub fn write<P: Packet>(&mut self, packet: &P) -> anyhow::Result<Vec<u8>> {
+        let serialized = postcard::to_allocvec(packet)?;
+        let mut buf = Vec::with_capacity(serialized.len() + 1);
+        buf.push(P::ID);
+        if let Some(cipher) = &self.cipher {
+            let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
+            buf = cipher
+                .encrypt(&nonce, serialized.as_slice())
+                .map_err(|_| anyhow::anyhow!("Encryption failed"))?;
+        } else {
+            buf.extend(serialized);
+        }
+        Ok(buf)
+    }
+
+    pub fn read<P: Packet>(&mut self, data: &[u8]) -> anyhow::Result<P> {
+        let buf = if let Some(cipher) = &self.cipher {
+            let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
+            cipher
+                .decrypt(&nonce, data)
+                .map_err(|_| anyhow::anyhow!("Decryption failed"))?
+        } else {
+            data.to_vec()
+        };
+        if buf.is_empty() || buf[0] != P::ID {
+            return Err(anyhow::anyhow!("Invalid packet ID"));
+        }
+        let packet: P = postcard::from_bytes(&buf[1..])?;
+        Ok(packet)
+    }
+
+    pub fn enable_encryption(&mut self, key: &[u8; 32]) -> anyhow::Result<()> {
+        self.cipher = Some(Aes256Gcm::new_from_slice(key).map_err(|_| anyhow::anyhow!("Invalid encryption key"))?);
+        Ok(())
+    }
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
-use agentduels_protocol::packets::HandshakePacket;
-use postcard::{from_bytes, to_allocvec};
+use agentduels_protocol::{packets::HandshakePacket, PacketCodec};
 use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::TcpStream};
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
+async fn main() {
     let mut socket = TcpStream::connect(("127.0.0.1", 8081)).await.expect("Failed to connect to game server");
+
+    let mut codec = PacketCodec::default();
 
     let packet = HandshakePacket {
         protocol_version: 1
     };
-    socket.write_all(&to_allocvec(&packet).unwrap()).await?;
+    socket.write_all(&codec.write(&packet).unwrap()).await.unwrap();
 
     let mut buf = [0; 64];
     socket.read(buf.as_mut_slice()).await.unwrap();
-    let packet: HandshakePacket = from_bytes(buf.as_slice()).unwrap();
+    let packet: HandshakePacket = codec.read(&buf).unwrap();
 
     println!("Other client has protocol version {}", packet.protocol_version);
-
-    Ok(())
 }
