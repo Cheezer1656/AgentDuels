9741d9d1ee7fff726e18f8856433d4f6c6febe79
Cheezer1656 <118921295+Cheezer1656@users.noreply.github.com>
2025-08-16 17:20:37 -0700

    Turn off anti aliasing

--- src/states/game/mod.rs
+++ src/states/game/mod.rs
@@ -31,6 +31,7 @@ fn replace_camera(mut commands: Commands, camera_query: Query<Entity, With<Camer
     }
     commands.spawn((
         Camera3d::default(),
+        Msaa::Off,
         Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
     ));
 }
