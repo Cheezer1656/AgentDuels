d7c1cd4064d071430e2e3e2df6fff67ec873f737
Cheezer1656 <118921295+Cheezer1656@users.noreply.github.com>
2025-10-12 14:46:31 -0700

    Make example client retry connections

--- example_client/client.py
+++ example_client/client.py
@@ -1,20 +1,29 @@
 import json
 from socket import AF_INET, socket
+from time import sleep
 
-ticks = 0
-with socket(AF_INET) as s:
-    s.connect(("127.0.0.1", 8082))
-    print("Connected!")
-    while True:
-        response = s.recv(1024)
-        if response == b"":
-            print("Server closed the connection.")
-            break
-        msg = json.loads(response.decode())
-        print("Received:", msg)
-        if next(iter(msg)) == "TickStart":
-            ticks += 1
-            msg = msg["TickStart"]
-            if ticks < 20:
-                s.send(b'{"MoveForward": null}')
-            s.send(b'{"EndTick": null}')
+while True:
+    ticks = 0
+    try:
+        with socket(AF_INET) as s:
+            s.connect(("127.0.0.1", 8082))
+            print("Connected!")
+            while True:
+                response = s.recv(1024)
+                if response == b"":
+                    print("Server closed the connection.")
+                    break
+                msg = json.loads(response.decode())
+                print("Received:", msg)
+                if next(iter(msg)) == "TickStart":
+                    ticks += 1
+                    msg = msg["TickStart"]
+                    if ticks < 20:
+                        s.send(b'{"MoveForward": null}')
+                    s.send(b'{"EndTick": null}')
+                    print("Sent actions")
+    except ConnectionRefusedError:
+        pass
+    except ConnectionResetError:
+        pass
+    sleep(1)
